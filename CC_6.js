daf969ba829fcd5c34ed8a09d06f27baa2ce879a
reiddedeaux <rdedeaux@usf.edu>
2025-02-09 17:28:15 -0500

    Task 3 - Employee Bonus Calculation

--- CC_6.js
+++ CC_6.js
@@ -19,4 +19,19 @@ let calculateSalesTax = function(amount, taxRate){
 console.log("Sales Tax: $", calculateSalesTax(100, .07));
 console.log("Sales Tax: $", calculateSalesTax(500, 0.1)); // logging the sales with sales tax into the console
 
+//Task 3 Employee Bonus Calculation
+let calculateBonus = (salary, performanceRating) =>{
+    let bonus;
+    if(performanceRating === "Excellent"){
+        bonus = salary * .20;    
+    } else if (performanceRating === "Good"){
+        bonus = salary * .10;
+    } else if(performanceRating === "Average"){
+        bonus = salary * .05;
+    }
+    return bonus;
+};
+
+console.log("Bonus 1: $", calculateBonus(5000, "Excellent"));
+console.log("Bonus 2: $", calculateBonus(7000, "Good")); //logging the salaries with the bonus based on their performance ratings
 
