b24f77ce50dfd583355783eb6f28678f33242d0b
reiddedeaux <rdedeaux@usf.edu>
2025-02-09 17:11:59 -0500

    Task 1 - Business Profit Calculation

--- CC_6.js
+++ CC_6.js
@@ -1,1 +1,13 @@
-//task 1
+//task 1 Business Profit Calculation
+
+function calculateProfit(costPrice, sellingPrice, unitsSold){
+    let profit =(sellingPrice - costPrice) * unitsSold; 
+    return profit;
+} // create a funtion to calculate profit
+calculateProfit(20, 30, 100); 
+calculateProfit(50, 70, 200);
+
+console.log("Total Profit: $", calculateProfit(20, 30, 100));
+console.log("Total Profit: $", calculateProfit(50, 70, 200)); //logging the resuluts of these functions
+
+
